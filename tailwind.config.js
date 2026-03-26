5e50ced29607c6d65688359c5bf350501e2029fa
bimoaji <bimoaji1271a7x@gmail.com>
2026-03-26 19:54:04 +0700

    create contact, footer & responsive layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,14 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '16px'
+      padding: '16px',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1290px',
+      },
     },
     extend: {
       colors: {
