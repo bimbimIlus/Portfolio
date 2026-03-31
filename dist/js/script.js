95c9f0db5957cae38a416cd5f49c15eea612a1db
bimoaji <bimoaji1271a7x@gmail.com>
2026-04-01 01:07:12 +0700

    Add dark mode toggle with fixed button at bottom right

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,3 +19,29 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// Button Dark-Light
+
+const button = document.getElementById('theme-toggle');
+const html = document.documentElement;
+
+// set awal
+if (localStorage.getItem('theme') === 'dark') {
+    html.classList.add('dark');
+    button.innerHTML = '☀️';
+} else {
+    button.innerHTML = '🌙';
+}
+
+// toggle klik
+button.addEventListener('click', () => {
+    html.classList.toggle('dark');
+
+    if (html.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+        button.innerHTML = '☀️';
+    } else {
+        localStorage.setItem('theme', 'light');
+        button.innerHTML = '🌙';
+    }
+});
